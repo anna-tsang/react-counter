6d4329a75f56ff6b50215b58666a5c1638a9c9c0
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-14 15:24:06 +0800

    [counter-react][Anna] feat: update to redux

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
+import {useDispatch} from "react-redux";
+import {UPDATE_SIZE} from "./constants/constants.js"
 
 
 function CounterSizeGenerator(props){
     const [size,setSize] = useState(0);
+    const dispatch = useDispatch();
 
     function handleChangeSize(event){
         var sizeValue = parseInt(event.target.value)
         setSize(sizeValue);
-        props.updateSize(sizeValue);
+        //props.updateSize(sizeValue);
+        dispatch({type: UPDATE_SIZE, payload: sizeValue});
     }
 
     return(
--- src/components/MultiCounter.js
+++ src/components/MultiCounter.js
@@ -1,30 +1,33 @@
 import CounterGroup from "./CounterGroup";
 import CounterSizeGenerator from "./CounterSizeGenerator";
 import CounterGroupSum from "./CounterGroupSum";
-import { useState } from "react";
+import { useSelector } from "react-redux";
 
 function MultiCounter(){
-    const [size, setSize] = useState(0);
-    const [sum, setSum] = useState(0);
+    const size = useSelector((state) => state.size);
+    const sum = useSelector((state) => state.sum);
 
-    function updateCounterSize(size){
-        setSize(size);
-    }
+    // const [size, setSize] = useState(0);
+    // const [sum, setSum] = useState(0);
 
-    function increase(){
-        setSum(sum + 1);
-    }
+    // function updateCounterSize(size){
+    //     setSize(size);
+    // }
 
-    function decrease(){
-        setSum(sum - 1);
-    }
+    // function increase(){
+    //     setSum(sum + 1);
+    // }
+
+    // function decrease(){
+    //     setSum(sum - 1);
+    // }
 
 
     return(
         <div>
-            <CounterSizeGenerator updateSize={updateCounterSize}></CounterSizeGenerator>
+            <CounterSizeGenerator></CounterSizeGenerator>
             <CounterGroupSum sum = {sum}></CounterGroupSum>
-            <CounterGroup size = {size} increase = {increase} decrease = {decrease}></CounterGroup>
+            <CounterGroup size = {size}></CounterGroup>
         </div>
         
     );
